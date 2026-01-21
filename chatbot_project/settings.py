0d164547fb74af399f2df413682092d102cf1015
Vignesh Padala <padalavignesh854@gmail.com>
2026-01-21 22:42:55 +0530

    Fix static files for Render

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -129,9 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / 'static'
 ]
+
 
 
 CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
